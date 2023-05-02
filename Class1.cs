1549ed37162281771b2c801855a2e43a3c83236c
faisalinayath <71935311+faisalinayath@users.noreply.github.com>
2023-05-02 14:37:22 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleConApp
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+
+            Console.WriteLine("Enter n");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter The Values of array");
+            for(int i = 0; i < n; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+            for(int j = 0; j < array.Length; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    continue;
+                }
+                else if (j % 2 != 0)
+                {
+                    Console.WriteLine(array[j]);
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
