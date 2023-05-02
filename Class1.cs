6bfcd65be61206fbd8700b6d0983d1143966ecda
faisalinayath <71935311+faisalinayath@users.noreply.github.com>
2023-05-02 15:12:54 +0530

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,62 +1,30 @@
-// The text “the quick and brown fox jumps over the lazy dog” contains all the 26 alphabets in English. 
-//We can generate a code for any letter in the following manner: 
-//The code is a combination two digits. The first digit represents the index of the word (first occurrence), which contains the letter and the second digit represents the index of that letter in the found word. 
-//For example, the letter “m” is found in the word “jumps” which is at index 5 in the sentence and in that word, “m” is found at index 2. Hence the code is 52. Similarly, the letter “v” is found in the word “over” which is at index 6 in the sentence and in that word, “v” is at index 
-
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace project
 {
-    class data
+    class Program
     {
-        public string sentence = "the quick and brown fox jumps over the lazy dog";
-    }
-
-    class hack
-    {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            List<int> lst = new List<int>();
-
-            Console.WriteLine("Enter The Word");
+            string[] main_sentence ={"the", "quick", "and" ,"brown" ,"fox" ,"jumps" ,"over" ,"the" ,"lazy" ,"dog"};
+            Console.WriteLine("Enter the word:");
             string word = Console.ReadLine();
-
-            foreach (var i in word)
-            {
-                data dataObj = new data();
-                string main_sentence = dataObj.sentence;
-
-                foreach (var z in main_sentence)
-                {
-                    string y = z.ToString();
-                    string x = i.ToString();
-                    if (y.Contains(x))
+            List<int> codes = new List<int>();
+            
+            foreach(char c in word){
+                 foreach(string x in main_sentence){
+                    if (x.Contains(c))
                     {
-                        int first = main_sentence.IndexOf(y);
-                        int second = y.IndexOf(x);
-
-                        string first_new = first.ToString();
-                        string second_new = second.ToString();
-
-                        string final = string.Concat(first_new, second_new);
-
-                        int final_int = Convert.ToInt32(final);
-
-                        lst.Add(final_int);
-
+                        int first=main_sentence.FindIndex(x);
+                        int second=x.IndexOf(c);
+                        
+                        Console.WriteLine(first);
                         break;
                     }
                 }
-
-
-            }
-
-            foreach (var x in lst)
-            {
-                Console.WriteLine(x);
-            }
+            }       
         }
     }
 }
