817dd63d687693650640c0db8caf96394e9c03f0
faisalinayath <71935311+faisalinayath@users.noreply.github.com>
2023-05-02 12:36:04 +0530

    Update Assignment5.cs

--- Assignment5.cs
+++ Assignment5.cs
@@ -1,108 +1,108 @@
-﻿//Create a CRUD based App for developing a Movie Database software where the user can add, remove and update movies of his Video library.
+//Create a CRUD based App for developing a Movie Database software where the user can add, remove and update movies of his Video library.
 //It should be a menu driven program that has 4 use cases for add, removing, finding and updating movie info in the application.
 
 using System;
 using System.Collections;
 using System.Collections.Generic;
 
-namespace movie
-{
-    class data
-    {
-        public string name { get; set; }
-
-        public List<string> movies { get; set; }
-
+namespace movie{
+    class data{
+        public string name{get;set;}
+        
+        public List<string> movies {get;set;}
+        
         public data()
         {
-            movies = new List<string>();
+            movies=new List<string>();
         }
     }
-
-    class create
-    {
-
-        public create()
+    
+    class create {
+        
+        public create(data myObj)
         {
-            data myObj = new data();
-            Console.WriteLine("Enter The Name Of The Movie");
-            myObj.movies.Add(Console.ReadLine());
+        Console.WriteLine("Enter The Name Of The Movie");
+        myObj.movies.Add(Console.ReadLine());
         }
     }
-
+    
     class read
     {
-        public read()
+        public read(data myObj)
         {
-            Console.WriteLine("The Movies Are :-");
-            data myObj1 = new data();
-            foreach (var x in myObj1.movies)
-            {
-                Console.WriteLine(x);
-            }
+        Console.WriteLine("The Movies Are :-");
+        
+        foreach(string x in myObj.movies)
+        {
+            Console.WriteLine(x);
+        }
         }
     }
-
+    
     class update
     {
-        public update()
+        public update(data myObj)
         {
-            data myObj2 = new data();
-
-            Console.WriteLine("Enter The Name Of The Movie You Want To Update");
-            var element = Console.ReadLine();
-            int index = myObj2.movies.FindIndex(a => a.Contains(element));
-
-            Console.WriteLine("Enter The Name Of The New Movie");
-            myObj2.movies[index] = Console.ReadLine();
-        }
+      
+        Console.WriteLine("Enter The Name Of The Movie You Want To Update");
+        var element=Console.ReadLine();
+        int index=myObj.movies.FindIndex(a=>a.Contains(element));
+        
+        Console.WriteLine("Enter The Name Of The New Movie");
+        myObj.movies[index]=Console.ReadLine();
     }
-
+    }
+    
     class delete
     {
-        public delete()
+        public delete(data myObj)
         {
-            data myObj3 = new data();
-            Console.WriteLine("Enter the movie name you want to delete");
-            var element = Console.ReadLine();
-            int index = myObj3.movies.FindIndex(a => a.Contains(element));
-            myObj3.movies.RemoveAt(index);
-        }
+     
+        Console.WriteLine("Enter the movie name you want to delete");
+        var element=Console.ReadLine();
+        int index=myObj.movies.FindIndex(a=>a.Contains(element));
+        myObj.movies.RemoveAt(index);
     }
-
-    class main
-    {
-        public static void Main(string[] args)
-        {
-
-            while (true)
+    }
+    
+    class main{
+        public static void Main(string[] args){
+            data myObj=new data();
+            
+            while(true){
+            Console.WriteLine("Enter\n1=>CREATE\n2=>READ\n3=>UPDATE\n4=>DELETE\n5=>exit");
+            
+            
+            
+            int x=Convert.ToInt32(Console.ReadLine());
+            switch(x)
             {
-                Console.WriteLine("Enter\n1=>CREATE\n2=>READ\n3=>UPDATE\n4=>DELETE\n5=>exit");
-
-
-
-                int x = Convert.ToInt32(Console.ReadLine());
-                switch (x)
-                {
-                    case 1:
-                        create createObj = new create();
-                        break;
-
-                    case 2:
-                        read readObj = new read();
-                        break;
-
-                    case 3:
-                        update updateObj = new update();
-                        break;
-
-                    case 4:
-                        delete deleteObj = new delete();
-                        break;
-
-                    case 5:
-                        break;
-                }
+                case 1:
+                create createObj=new create(myObj);
+                break;
+                
+                case 2:
+                read readObj=new read(myObj);
+                break;
+                
+                case 3:
+                update updateObj=new update(myObj);
+                break;
+                
+                case 4:
+                delete deleteObj=new delete(myObj);
+                break;
+                
+                default:
+                Console.WriteLine("Invalid Input");
+                break;
+            }
+            
+            if(x==5)
+            {
+                break;
+            }
+            
             }
         }
     }
